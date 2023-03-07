ec1065685163ac56bb905ca0ed2d8795e6624905
Kostiantyn Varetsa <k.varetsa@gmail.com>
2023-03-08 00:08:12 +0400

    Made some changes: returned original function of Editing in conjunction with file operation

--- new_todo_list/new_todo_list.py
+++ new_todo_list/new_todo_list.py
@@ -1,28 +1,52 @@
 
-def view():
-    todo_list = open("to_do.txt", "r")
-    todo_list.seek(0)
-    for task in todo_list:
-        print(task, end="")
-    todo_list.close()
+def read(tasks):
+    todo = open("to_do.txt", "r")
+    todo.seek(0)
+    for task in todo:
+        tasks.append(task[:-1])
+    todo.close()
 
 
-def add():
-    todo_list = open("to_do.txt", "a+")
-    todo_list.write(input("Your task here: "))
-    todo_list.write("\n")
-    todo_list.close()
+def write(tasks):
+    todo = open("to_do.txt", "w")
+    for item in tasks:
+        todo.write(f"{item}\n")
+    todo.close()
+
+
+def view(tasks):
+    for item in range(len(tasks)):
+        print(f"{item + 1}. {tasks[item]}")
+
+
+def add(tasks):
+    new = input(">>> ")
+    tasks.append(new)
+
+
+def edit(tasks):
+    view(todo_list)
+    try:
+        num = int(input("What task do you want to mark as DONE? Enter a number: "))
+        tasks[num - 1] = f"\033[9m{tasks[num - 1]}\033[0m"
+    except ValueError as ex:
+        print(ex)
 
 
 if __name__ == "__main__":
+    todo_list = []
+    read(todo_list)
     while True:
-        menu = input("Choose action (Add, View, Quit): ").lower()
+        menu = input("Choose action (Add, Edit, View, Quit): ").lower()
         try:
             if menu[0] == "a":
-                add()
+                add(todo_list)
+            elif menu[0] == "e":
+                edit(todo_list)
             elif menu[0] == "v":
-                view()
+                view(todo_list)
             elif menu[0] == "q":
+                write(todo_list)
                 break
             else:
                 print("Wrong command!")
