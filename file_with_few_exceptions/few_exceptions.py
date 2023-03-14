f8bdabf24158113ca26475f5fd08e869d241e492
Kostiantyn Varetsa <k.varetsa@gmail.com>
2023-03-14 23:59:44 +0400

    Changed the code of the file_with_few_exceptions accorfing to the comments

--- file_with_few_exceptions/few_exceptions.py
+++ file_with_few_exceptions/few_exceptions.py
@@ -1,34 +1,21 @@
 
-def write():
-    file = open("divisor.txt", "w")
+def read_n_write(our_file):
+    num = input("Choose a number: ")
     divisor = input("Type in a divisor: ")
-    file.write(divisor)
-    file.close()
-
-
-def read():
-    file_name = input("What file do you want to open: ")
+    our_file.write(divisor)
+    our_file.seek(0)
     try:
-        file = open(f"{file_name}.txt", "r")
-        try:
-            print(num / int(file.read()))
-            change_divisor = input("Do you want to change the divisor: ")
-            if change_divisor == "yes":
-                file.write(input("Type in a divisor: "))
-        except (PermissionError, ValueError) as errors:
-            print(errors)
-        finally:
-            file.close()
-    except FileNotFoundError as error:
-        print(error)
+        print(int(num) / int(our_file.read()))
+    except ValueError as e:
+        print(e)
     finally:
+        our_file.close()
         print("The end.")
 
 
 if __name__ == "__main__":
     try:
-        num = int(input("Choose a number: "))
-    except ValueError:
-        num = 100
-    write()
-    read()
+        file = open("divisor.txt", "r+")
+        read_n_write(file)
+    except (FileNotFoundError, PermissionError) as errors:
+        print(errors)
