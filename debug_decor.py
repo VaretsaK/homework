6ba454983dde47401a7c05469d55368ec5155bd0
Kostiantyn Varetsa <k.varetsa@gmail.com>
2023-03-21 17:24:58 +0400

    Changed code according to the comment...result is saved and checking whether we want to se it or not

--- debug_decor.py
+++ debug_decor.py
@@ -1,21 +1,29 @@
 
-def debug_maker(print_input, print_output):
+def debug_maker(print_output):
 
     def debug(func):
+        res = []
+        count = 0
 
         def simple(*args, **kwargs):
-            if print_input:
-                print(f"Input: {(tuple(args) + tuple(kwargs.keys()))}")
+            nonlocal res, count
+            res.append(func(*args, **kwargs))
+            print(f"Input: {(tuple(args) + tuple(kwargs.keys()))}")
             if print_output:
-                func(*args, **kwargs)
+                print(res[count])
+            count += 1
+        # Ти повинен виконувати функцію у будь-якому випадку, просто ти
+        # повинен записувати результат окремо і виводити лише якщо print_output вказано
         return simple
     return debug
 
 
-@debug_maker(print_input=True, print_output=True)
+@debug_maker(print_output=True)
 def mult(a, b):
-    print(a * b)
+    return a * b
 
 
 if __name__ == "__main__":
     mult(5, 6)
+    mult(5, 3)
+    mult(25, 6)
